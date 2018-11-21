a61101bafd9e1a66607e4483015bafdd96735133
min0r2468 <minor_2468@hotmail.com>
2018-11-21 18:26:30 +0000

    Added a bit more documentation and made it work on windows (For some reason I had to encode it to UTF-8 on windows, but not on linux?)

--- mangaread.py
+++ mangaread.py
@@ -3,10 +3,11 @@ from bs4 import BeautifulSoup as BSoup
 import lxml
 import sys
 import os #this is needed to allow the user to make their own custom directory
+import io
 
 #first set up the global variables
 BASE_URL = "https://www.wuxiaworld.com"
-DEFAULT_DIRECTORY = "stores"
+DEFAULT_DIRECTORY = "stories"
 
 def get_all_tags(url, element_class="", tag="a"):
 	#General purpose function for getting all tags from a page
@@ -23,16 +24,23 @@ def get_story(url):
 	#go through each chapter, and get the text
 	return '\n'.join([get_all_tags(BASE_URL+i, "fr-view", "div")[0].text for i in chapters])
 
+def print_help( ):
+	print("Usage: python mangaread.py <Directory that you want to save the stories in>")
+
 def main( ):
 	try:
 		directory = sys.argv[1]
+		if directory=="-h":
+			print_help( )
+			exit(0)
 	except IndexError:
 		directory = DEFAULT_DIRECTORY
-	os.system("mkdir "+directory)
+	os.system("mkdir "+directory) #reEE command injection vulnerability
 	stories = get_all_tags(BASE_URL+"/tag/completed", "text-white")
 	for i, story in enumerate(stories): #(use enumerate so I can have more verbosity)
 		print("[*] Getting {}, story number {}".format(story.text.strip(), i+1))
-		with open("{}/{}.txt".format(directory, story.text.strip()), "w") as f:
+		#For some reason using open doesn't work on windows, but usign io.open and specifying an encoding does.
+		with io.open("{}/{}.txt".format(directory, story.text.strip()), "w", encoding="utf-8") as f:
 			f.write(str(get_story(story["href"])))
 
 if __name__=='__main__':
