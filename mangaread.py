6eadb710285614418d1340ec564ffb92c9940022
TristanRice <tristan@24marketing.co.uk>
2018-11-21 11:40:19 +0000

    Fin

--- mangaread.py
+++ mangaread.py
@@ -1,37 +1,39 @@
 import requests
 from bs4 import BeautifulSoup as BSoup
 import lxml
-import re
+import sys
+import os #this is needed to allow the user to make their own custom directory
 
 #first set up the global variables
 BASE_URL = "https://www.wuxiaworld.com"
+DEFAULT_DIRECTORY = "stores"
 
 def get_all_tags(url, element_class="", tag="a"):
 	#General purpose function for getting all tags from a page
+	print("[*] Getting data from {}".format(url))
 	r = requests.get(url)
 	soup = BSoup(r.text, "lxml")
 	return soup.find_all(tag, class_=element_class)
 
-class Story:
-
-	def __init__(self, url):
-		self.url = BASE_URL+url
-		self.chapters = self.get_chapters( )
-
-	def get_chapters(self):
-		all_links = get_all_tags(self.url, element_class="chapter-item", tag="li")
-		return [li.findChildren("a")[0]["href"] for li in all_links]
-		
-	def get_text(self):
-		return ''.join([get_all_tags(BASE_URL+i, "fr-view", "div")[0].text 
-					   for i in self.chapters])
+def get_story(url):
+	#frst get the URLs for each chapter in the story 
+	all_links = get_all_tags(BASE_URL+url, element_class="chapter-item", tag="li")
+	#since the a tags have no classes, I need to get the li tags taht they are under
+	chapters  = [li.findChildren("a")[0]["href"] for li in all_links]
+	#go through each chapter, and get the text
+	return '\n'.join([get_all_tags(BASE_URL+i, "fr-view", "div")[0].text for i in chapters])
 
 def main( ):
+	try:
+		directory = sys.argv[1]
+	except IndexError:
+		directory = DEFAULT_DIRECTORY
+	os.system("mkdir "+directory)
 	stories = get_all_tags(BASE_URL+"/tag/completed", "text-white")
-	for story in stories:
-		s = Story(story["href"])
-		with open(story.text.strip( )+".txt", "w") as f:
-			f.write(str(s.get_text( )))
+	for i, story in enumerate(stories): #(use enumerate so I can have more verbosity)
+		print("[*] Getting {}, story number {}".format(story.text.strip(), i+1))
+		with open("{}/{}.txt".format(directory, story.text.strip()), "w") as f:
+			f.write(str(get_story(story["href"])))
 
 if __name__=='__main__':
 	main( )
